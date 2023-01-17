80e2cc07fb702e96bd1eafcd2b00713b9eabc489
Christerresin <christer.resin@gmail.com>
2023-01-17 10:20:06 +0100

    inital commit

---
+++ RunGroupWebApp/Models/AppUser.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RunGroupWebApp.Models
+{
+    public class AppUser
+    {
+        [Key]
+        public int Id { get; set; }
+        public int? Pace { get; set; }
+        public int? Mileage { get; set; }
+        public string? ProfileImageUrl { get; set; }
+        public string? City { get; set; }
+        public string? State { get; set; }
+        [ForeignKey("Address")]
+        public int? AddressId { get; set; }
+        public Address? Address { get; set; }
+        //public List<Club>? Clubs { get; set; }
+        //public List<Club>? Races { get; set; }
+    }
+}
