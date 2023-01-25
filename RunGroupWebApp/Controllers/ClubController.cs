269284e5b79e7ef375cf4455ffbe3a8d69f7611b
Christerresin <christer.resin@gmail.com>
2023-01-25 10:08:31 +0100

    add delete route and button for club

--- RunGroupWebApp/Controllers/ClubController.cs
+++ RunGroupWebApp/Controllers/ClubController.cs
@@ -134,5 +134,23 @@ namespace RunGroupWebApp.Controllers
             
         }
 
+        public async Task<IActionResult> Delete(int id)
+        {
+            var clubDetails = await _clubRepository.GetClubById(id);
+            if (clubDetails == null) return View("Error");
+
+            return View(clubDetails);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteClub(int id)
+        {
+            var clubDetails = await _clubRepository.GetClubById(id);
+            if (clubDetails == null) return View("Error");
+
+            _clubRepository.Delete(clubDetails);
+            return RedirectToAction("Index");
+        }
+
     }
 }
